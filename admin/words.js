225f19432108a3e25ac83418ea795c78fdcfa365
Kamran Mustafayev <gokturk413@gmail.com>
2022-12-01 12:32:26 +0400

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,43 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+    'my-opcua adapter settings': {
+        'en': 'Adapter settings for my-opcua',
+        'de': 'Adaptereinstellungen für my-opcua',
+        'ru': 'Настройки адаптера для my-opcua',
+        'pt': 'Configurações do adaptador para my-opcua',
+        'nl': 'Adapterinstellingen voor my-opcua',
+        'fr': "Paramètres d'adaptateur pour my-opcua",
+        'it': "Impostazioni dell'adattatore per my-opcua",
+        'es': 'Ajustes del adaptador para my-opcua',
+        'pl': 'Ustawienia adaptera dla my-opcua',
+        'zh-cn': 'my-opcua的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'вариант 2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
